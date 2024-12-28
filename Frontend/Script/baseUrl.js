7730270a77169056495abe2acb8040b96332e150
aditi pathak <https://github.com/26komalagrawal>
2024-12-28 14:51:21 +0530

    Creating a DOM Book App for user understanding

---
+++ Frontend/Script/baseUrl.js
@@ -0,0 +1,1 @@
+export const baseUrl = "https://admitted-gem-thistle.glitch.me";
---
+++ Frontend/Script/navbar.js
@@ -0,0 +1,8 @@
+const navbar =`
+    <div id="nav">
+        <a href="index.html">Home</a>
+        <a href="admin.html">Admin</a>
+        <a href="book.html">Books</a>
+    </div>
+`;
+document.getElementById("#nav").innerHTML = navbar;
